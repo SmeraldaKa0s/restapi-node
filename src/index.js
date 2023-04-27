19789e2a4c893099e29d3e4a1a2c08c4834bdf74
SmeraldaKa0s <anabella.lerner@gmail.com>
2023-04-27 15:29:41 -0300

    agrego archivo routes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan'); 
 
+//  settings
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2);
+
 //  middleware
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+//  routes
+app.use(require('./routes/index'));
 
 //  starting the server 
-app.listen(3000, () => {
-    console.log(`Server on port ${3000}`);
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
 }); 
 
